9297086397308a959177276be1adf9cae240cec3
Johnny Squardov <johnny@johnmichael.dev>
2021-01-22 15:33:06 -0500

    Use Devise to create login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root "pages#my_todo_items", as: :authenticated_root
+  end
   root "pages#home"
   get "pages/my_todo_items"
 end
